1de8f0b75dccff3320ab59ddabbb5ac476302632
Vitor Henckel <vhenckel627@hotmail.com>
2019-02-08 11:46:31 -0200

    Ajustando as rotas e menu

---
+++ src/NewSeries.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+import api from './Api'
+
+class NewSeries extends Component {
+  render() {
+    return <section className="intro-section"><h1>Nova Série</h1></section>
+  }
+}
+export default NewSeries
